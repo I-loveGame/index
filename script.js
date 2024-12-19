980bafd193f5bbbc73cd9a70b5acf85e497ca2fb
I-loveGame <nick.bespalov.nbv@gmail.com>
2024-12-19 23:19:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,59 +3,50 @@ document.addEventListener('DOMContentLoaded', () => {
     const checkButton = document.getElementById('checkButton');
     const resultDiv = document.getElementById('result');
     const steamInfoDiv = document.getElementById('steamInfo');
-    const confirmButton = document.getElementById('confirmButton');
+    const instructionButton = document.getElementById('instructionButton');
+    const backButton = document.getElementById('backButton');
+     const container = document.getElementById('container');
+    const instructionScreen = document.getElementById('instructionScreen');
+
     const tg = window.Telegram.WebApp;
 
-    checkButton.addEventListener('click', async () => {
+    instructionButton.addEventListener('click', () => {
+         container.style.transform = 'translateX(-100%)';
+         instructionScreen.style.display = 'block';
+          backButton.style.display = 'block';
+          setTimeout(() => {
+            instructionScreen.style.opacity = 1;
+           }, 0) //что бы анимация работала
+     });
+
+
+     backButton.addEventListener('click', () => {
+         instructionScreen.style.opacity = 0;
+           setTimeout(() => {
+                container.style.transform = 'translateX(0)';
+                instructionScreen.style.display = 'none';
+                 backButton.style.display = 'none';
+           }, 300) //что бы анимация работала
+    });
+
+
+    checkButton.addEventListener('click', () => {
         const steamId = steamIdInput.value.trim();
         if (steamId) {
             if (!/^[0-9]{17}$/.test(steamId)) {
                 resultDiv.textContent = 'Неверный формат Steam ID. Должно быть 17 цифр.';
                 return;
             }
-            resultDiv.textContent = "Загрузка информации об аккаунте...";
-            try {
-                 const response = await fetch(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=F8A10F215FECA2977A762232DEA544F3&steamids=${steamId}`);
-                  if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                 }
 
-                const data = await response.json();
-                 if (data && data.response && data.response.players && data.response.players.length > 0) {
-                    const player = data.response.players[0];
-                    steamInfoDiv.innerHTML = `
-                         <img src="${player.avatarfull}" alt="Аватарка">
-                         <p>Имя: ${player.personaname}</p>
-                         <p>Steam ID: ${player.steamid}</p>
-                        <p>Это ваш аккаунт?</p>
-                    `;
-                    confirmButton.style.display = 'block';
-                    resultDiv.textContent = ""; //очищаем сообщение
-                } else {
-                  resultDiv.textContent = 'Аккаунт не найден';
-                  steamInfoDiv.innerHTML = "";
-                  confirmButton.style.display = "none";
-
-                 }
-            }  catch (error) {
-                 resultDiv.textContent = `Ошибка получения данных: ${error.message}`
-                 console.error('Ошибка получения данных:', error);
-                steamInfoDiv.innerHTML = "";
-                confirmButton.style.display = "none";
-           }
+            // Отправляем данные в бота
+            const data = { steamId };
+            const jsonString = JSON.stringify(data);
+            console.log("Данные для отправки:", jsonString);
+            tg.sendData(jsonString);
+            resultDiv.textContent = "Отправлено на проверку...";
+            checkButton.disabled = true;
         } else {
-             resultDiv.textContent = 'Введите Steam ID.';
+            resultDiv.textContent = 'Введите Steam ID.';
         }
-    });
-
-     confirmButton.addEventListener('click', () => {
-        const steamId = steamIdInput.value.trim();
-        const data = { steamId: steamId };
-        const jsonString = JSON.stringify(data);
-        console.log('Sending data:', jsonString);
-        tg.sendData(jsonString);
-        resultDiv.textContent = "Отправлено!";
-        confirmButton.style.display = 'none';
-        checkButton.disabled = true;
     });
 });
