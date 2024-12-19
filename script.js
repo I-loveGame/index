f1521e56c10bdc7dbbcd084e6e52d4265a2714fe
I-loveGame <nick.bespalov.nbv@gmail.com>
2024-12-19 21:50:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,33 +7,33 @@ document.addEventListener('DOMContentLoaded', function() {
         tg.ready();
         console.log("WebApp API is assumed to be available.");
     } else {
-         console.log("Telegram WebApp API might not be available");
+        console.log("Telegram WebApp API might not be available.");
     }
 
     const sendButton = document.getElementById("sendButton");
     const textInput = document.getElementById("textInput");
-    if (sendButton) {
-       sendButton.addEventListener("click", () => {
+    if (sendButton && textInput) {
+        sendButton.addEventListener("click", () => {
             console.log("Кнопка 'Отправить' нажата.");
             const text = textInput.value.trim();
             if (text) {
-                const data = { text: text };
-                const jsonString = JSON.stringify(data);
-                if (tg) {
+               const data = { text: text };
+               const jsonString = JSON.stringify(data);
+               if (tg) {
                     tg.sendData(jsonString);
-                    console.log("Отправлены данные:", jsonString);
+                     console.log("Отправлены данные:", jsonString);
                 } else {
-                    console.log("Telegram WebApp API is not available.");
+                   console.log("Telegram WebApp API is not available.");
                 }
+
             } else {
+                console.log("Введите текст");
                 alert("Пожалуйста, введите текст.");
             }
         });
-    } else {
-        console.log("Кнопка 'Отправить' не найдена!");
     }
-
-     if (!textInput) {
-         console.log("Поле ввода не найдено")
-     }
+    else
+     {
+        console.error("Кнопка или поле ввода не найдены");
+    }
 });
