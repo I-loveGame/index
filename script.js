184a8845ba414e9cdf7cd9a12c27ccaef3350f65
I-loveGame <nick.bespalov.nbv@gmail.com>
2024-12-19 23:27:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,46 +2,45 @@ document.addEventListener('DOMContentLoaded', () => {
     const steamIdInput = document.getElementById('steamIdInput');
     const checkButton = document.getElementById('checkButton');
     const resultDiv = document.getElementById('result');
-    const steamInfoDiv = document.getElementById('steamInfo');
+     const steamInfoDiv = document.getElementById('steamInfo');
     const instructionButton = document.getElementById('instructionButton');
     const backButton = document.getElementById('backButton');
-     const container = document.getElementById('container');
-    const instructionScreen = document.getElementById('instructionScreen');
+    const container = document.getElementById('container');
+     const instructionScreen = document.getElementById('instructionScreen');
+
 
     const tg = window.Telegram.WebApp;
 
-    instructionButton.addEventListener('click', () => {
+
+     instructionButton.addEventListener('click', () => {
          container.style.transform = 'translateX(-100%)';
          instructionScreen.style.display = 'block';
           backButton.style.display = 'block';
           setTimeout(() => {
             instructionScreen.style.opacity = 1;
-           }, 0) //что бы анимация работала
+           }, 0)
      });
 
-
      backButton.addEventListener('click', () => {
-         instructionScreen.style.opacity = 0;
+        instructionScreen.style.opacity = 0;
            setTimeout(() => {
-                container.style.transform = 'translateX(0)';
-                instructionScreen.style.display = 'none';
-                 backButton.style.display = 'none';
-           }, 300) //что бы анимация работала
+              container.style.transform = 'translateX(0)';
+              instructionScreen.style.display = 'none';
+              backButton.style.display = 'none';
+          }, 300)
     });
 
 
-    checkButton.addEventListener('click', () => {
+   checkButton.addEventListener('click', () => {
         const steamId = steamIdInput.value.trim();
         if (steamId) {
             if (!/^[0-9]{17}$/.test(steamId)) {
-                resultDiv.textContent = 'Неверный формат Steam ID. Должно быть 17 цифр.';
-                return;
+                 resultDiv.textContent = 'Неверный формат Steam ID. Должно быть 17 цифр.';
+                 return;
             }
-
-            // Отправляем данные в бота
-            const data = { steamId };
-            const jsonString = JSON.stringify(data);
-            console.log("Данные для отправки:", jsonString);
+              const data = { steamId };
+               const jsonString = JSON.stringify(data);
+             console.log("Данные для отправки:", jsonString);
             tg.sendData(jsonString);
             resultDiv.textContent = "Отправлено на проверку...";
             checkButton.disabled = true;
